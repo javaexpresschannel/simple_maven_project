9a36ee68eb42fabe574b4460a04d804c560848fc
javaexpresschannel <javaexpresschannel@gmail.com>
2024-04-02 16:47:17 +0530

    first commit

---
+++ src/main/java/com/javaexpress/SimpleMavenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.javaexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMavenProjectApplication.class, args);
+	}
+
+}
